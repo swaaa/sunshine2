356f76f23a66f82d61e7aab50ad8d4beb994d40f
swaaa <mike@von-dem-brinke.de>
2015-07-25 16:30:28 +0200

    first commit: everything happened so far

--- java/com/example/android/sunshine/app/MainActivity.java
+++ java/com/example/android/sunshine/app/MainActivity.java
@@ -37,8 +37,7 @@ public class MainActivity extends ActionBarActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             // Intent starts new activity
-            Intent showSettings = new Intent(this, SettingsActivity.class);
-            startActivity(showSettings);
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
